7db2263b931f943daefd98b8ff84785741e789c1
ShinEi4 <harisbrandon44@gmail.com>
2024-05-07 08:48:27 +0200

    modifFrontController

---
+++ mg/ituprom16/FrontController.java
@@ -0,0 +1,40 @@
+package mg.ituprom16;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.*;
+public class FrontController extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try{
+            processRequest(request, response);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+        try{
+            processRequest(request, response);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void processRequest(HttpServletRequest request,HttpServletResponse response) throws Exception
+    {
+        response.setContentType("text/html");
+        PrintWriter out=response.getWritter();
+        out.println("<html>");
+        out.println("<head><title>Print URL</title></head>");
+        out.println("<body>");
+        out.println("<h1>URL à imprimer :</h1>");
+        out.println("<p>" + request.getRequestURL() + "</p>");
+        out.println("</body></html>");
+    }
+    
+}
