e34e09fbfdece56dfaf52499283d6bf63fbc30b9
ShinEi4 <harisbrandon44@gmail.com>
2024-05-24 16:19:31 +0200

    finition

---
+++ src/mg/ituprom16/Mapping.java
@@ -0,0 +1,24 @@
+package mg.ituprom16;
+
+public class Mapping {
+    String className;
+    String methodName;
+
+    public Mapping(String className, String methodName) {
+        this.setClassName(className);
+        this.setMethodName(methodName);
+    }
+
+    public String getClassName() {
+        return className;
+    }
+    public void setClassName(String className) {
+        this.className = className;
+    }
+    public String getMethodName() {
+        return methodName;
+    }
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+}
--- src/mg/ituprom16/controller/FrontController.java
+++ src/mg/ituprom16/controller/FrontController.java
@@ -1,106 +1,119 @@
 package mg.ituprom16.controller;
 
 import jakarta.servlet.http.*;
+import mg.ituprom16.Mapping;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.ModuleLayer.Controller;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
 import jakarta.servlet.*;
 public class FrontController extends HttpServlet{
+    
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try{
-            processRequest(request, response);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
     }
 
-    protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
-        try{
-            processRequest(request, response);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
     }
 
-    // public void processRequest(HttpServletRequest request,HttpServletResponse response) throws Exception
-    // {
-    //     response.setContentType("text/html");
-    //     PrintWriter out=response.getWriter();
-    //     out.println("<html>");
-    //     out.println("<head><title>Print URL</title></head>");
-    //     out.println("<body>");
-    //     out.println("<h1>URL à imprimer :</h1>");
-    //     out.println("<p>" + request.getRequestURL() + "</p>");
-    //     out.println("</body></html>");
-    // }
-    private boolean checked = false;
-    private List<Class<?>> controllerClasses = new ArrayList<>();
+    private HashMap<String, Mapping> urlMappings = new HashMap<>();
+    protected ArrayList<String> listeControlleurs = new ArrayList<String>();
 
-    public void findControllerClasses() {
-        String controllerPackage = getServletConfig().getInitParameter("controller");
-        if (controllerPackage == null || controllerPackage.isEmpty()) {
-            System.err.println("Controller package not specified");
-            return;
-        }
-        ServletContext servletContext = getServletContext();
-        String directoryPath = servletContext.getRealPath(controllerPackage);
+    public void getListeControlleurs(String packagename) throws Exception {
+        String bin_path = "WEB-INF/classes/" + packagename.replace(".", "/");
+        bin_path = getServletContext().getRealPath(bin_path);
 
-        File directory = new File(directoryPath);
-
-        if (!directory.exists() || !directory.isDirectory()) {
-            System.err.println("Package directory not found: " + directory.getAbsolutePath());
-            return;
-        }
-
-        findClassesInDirectory(controllerPackage, directory);
-        checked = true;
-    }
-
-    private void findClassesInDirectory(String packageName, File directory) {
-        for (File file : Objects.requireNonNull(directory.listFiles())) {
-            if (file.isDirectory()) {
-                findClassesInDirectory(packageName + "." + file.getName(), file);
-            } else if (file.getName().endsWith(".class")) {
-                String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
-                addClassIfController(className);
+        File b = new File(bin_path);
+        for (File fichier : b.listFiles()) {
+            if (fichier.isFile() && fichier.getName().endsWith(".class")) {
+                String className = packagename + "." + fichier.getName().replace(".class", "");
+                Class<?> classe = Class.forName(className);
+                if (classe.isAnnotationPresent(Controller.class)) {
+                    for (Method method : classe.getDeclaredMethods()) {
+                        if (method.isAnnotationPresent(Get.class)) {
+                            Get getAnnotation = method.getAnnotation(Get.class);
+                            String url = getAnnotation.value();
+                            Mapping mapping = new Mapping(classe.getName(), method.getName());
+                            urlMappings.put(url, mapping);
+                        }
+                    }
+                }
             }
         }
     }
 
-    private void addClassIfController(String className) {
+    @Override
+    public void init() throws ServletException {
+        super.init();
         try {
-            Class<?> clazz = Class.forName(className);
-            if (clazz.isAnnotationPresent(AnnotationController.class)) {
-                controllerClasses.add(clazz);
-            }
-        } catch (ClassNotFoundException e) {
-            System.err.println("Class not found: " + className);
-        }
-    }
-
-    protected void processRequest(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        PrintWriter out = res.getWriter();
-        try {
-            String url = req.getRequestURL().toString();
-            out.println("URL: " + url);
-
-            if (!checked) {
-                findControllerClasses();
-            }
-
-            out.println("Liste des classes controleurs :");
-            for (Class<?> controllerClass : controllerClasses) {
-                out.println(controllerClass.getName());
-            }
+            getListeControlleurs(getServletContext().getInitParameter("controllerPackage"));
         } catch (Exception e) {
             e.printStackTrace();
-            throw e;
+        }
+    }
+
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException {
+        String contextPath = req.getContextPath(); // Obtenir le contexte de l'application
+        String urlPath = req.getRequestURI().substring(contextPath.length()); // Enlever le contexte de l'application        
+        if (urlPath == null || urlPath.isEmpty()) {
+            resp.getWriter().println("No URL path provided");
+            return;
+        }
+
+        Mapping mapping = urlMappings.get(urlPath);
+        PrintWriter out = resp.getWriter();
+        try {
+               // Afficher le contenu du HashMap urlMappings
+               out.println("Contenu de urlMappings:");
+               for (HashMap.Entry<String, Mapping> entry : urlMappings.entrySet()) {
+                   String url = entry.getKey();
+                   Mapping mapping2 = entry.getValue();
+                   out.println("URL: " + url + " - Class: " + mapping2.getClassName() + ", Method: " + mapping2.getMethodName());
+               }
+
+                getListeControlleurs(getServletContext().getInitParameter("controllerPackage"));
+                resp.setContentType("text/html;charset=UTF-8");
+                if (mapping != null) {
+                    out.println("<!DOCTYPE html>");
+                    out.println("<html>");
+                    out.println("<head>");
+                    out.println("<title>Mapping Information</title>");
+                    out.println("</head>");
+                    out.println("<body>");
+                    out.println("<h1>Information for URL: " + urlPath + "</h1>");
+                    out.println("<ul>");
+                    out.println("<li>Class Name: " + mapping.getClassName() + "</li>");
+                    out.println("<li>Method Name: " + mapping.getMethodName() + "</li>");
+                    out.println("</ul>");
+                    out.println("</body>");
+                    out.println("</html>");
+                } else {
+                    out.println("<!DOCTYPE html>");
+                    out.println("<html>");
+                    out.println("<head>");
+                    out.println("<title>No Mapping Found</title>");
+                    out.println("</head>");
+                    out.println("<body>");
+                    out.println("<h1>No method associated with this URL path: " + urlPath + "</h1>");
+                    out.println("</body>");
+                    out.println("</html>");
+                }            
+        } catch (Exception e) {
+            out.println(e.getMessage());
+            for (StackTraceElement ste : e.getStackTrace()) {
+                out.println(ste);
+            }
         }
     }
     
